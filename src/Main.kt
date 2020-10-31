3b0c542fd386c89e5b27519d87f33d10d370df70
Maryam Haghgou <artmary.ir@gmail.com>
2020-11-01 02:22:03 +0330

    kt init.

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("hello kotlin")
+//   constant with val, variable with var
+    val name:String = "Mary"
+    var userName:String = "artMary"
+    println(name)
+    println(userName)
+}
