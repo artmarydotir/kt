3da5db09370c6a30292c6bb3d96b36db6633b520
Maryam Haghgou <artmary.ir@gmail.com>
2020-11-01 02:45:37 +0330

    statement.

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,27 @@
+//top-level var
+var greeting:String = "Welcome"
+
+//define null value
+var greet:String? = null
+
 fun main() {
     println("hello kotlin")
-//   constant with val, variable with var
+  // constant with val, variable with var
     val name:String = "Mary"
     var userName:String = "artMary"
     println(name)
     println(userName)
+    println(greet)
+  // ##### if statement
+    if (greet !== null) {
+        println(greet)
+    } else {
+        println("im else")
+    }
+
+    // #### when statement like switch case
+    when(greet) {
+        null -> println("case null")
+        else -> println("when case else")
+    }
 }
