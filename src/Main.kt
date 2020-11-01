b1d338deb275315ae3e772a15c97a5bd76475d7e
Maryam Haghgou <artmary.ir@gmail.com>
2020-11-02 02:22:59 +0330

    func

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,24 @@ var greeting:String = "Welcome"
 //define null value
 var greet:String? = null
 
+//define func
+fun sayWelcome() :String {
+    return "Hello dear From function "
+}
+//func that don't return anything use unit
+fun sayBye(): Unit {
+    println(sayWelcome())
+}
+// func return nullable string
+fun returnNull(): String? {
+return  "nulliii "
+}
+//func with params
+fun funParams(item:String) {
+    val msg = "Hello $item func params"
+    println(msg)
+}
+
 fun main() {
     println("hello kotlin")
   // constant with val, variable with var
@@ -24,4 +42,17 @@ fun main() {
         null -> println("case null")
         else -> println("when case else")
     }
+
+//    val greetingToPrint = if(greet !== null) greet else "hi"
+//    val greetingToPrint = when(greet) {
+//        null-> "hi"
+//        else-> greet
+//    }
+//    println(greetingToPrint)
+
+//    invoke function
+    println(sayWelcome())
+    println(returnNull())
+    sayBye()
+    funParams("Love Kotlin")
 }
